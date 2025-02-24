2e851d531e9ea926bfc770f214c1479b56f63f56
eak <eak@ivalua.com>
2025-02-24 22:32:18 +0000

    Enhance TextAugmentation class with punctuation removal; improve logging format and update model loading logic for better readability and organization

--- train_classification.py
+++ train_classification.py
@@ -11,11 +11,14 @@ from transformers import (
     BertTokenizerFast,
     TrainingArguments,
     Trainer,
+    EarlyStoppingCallback,
 )
 from evaluator import ComputeMetric
 
 # Set up logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 # Set environment variables
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
@@ -28,17 +31,25 @@ train_batch_size = 256
 
 
 class TextAugmentation:
-    def __init__(self, remove_digits=0.1, shuffle_words=0.5, remove_words=0.2, include_digits=0.3):
+    def __init__(
+        self,
+        remove_digits=0.1,
+        shuffle_words=0.5,
+        remove_words=0.2,
+        include_digits=0.3,
+        ponctuation=0.2,
+    ):
         self.prob_remove_digits = remove_digits
         self.prob_shuffle_words = shuffle_words
         self.prob_remove_words = remove_words
         self.prob_include_digits = include_digits
+        self.ponctuation = ponctuation
 
     def __call__(self, text: str):
         if random.random() < self.prob_include_digits:
             text += str(random.randint(0, 10000))
         if random.random() < self.prob_remove_digits:
-            text = re.sub(r'\d', '', text)
+            text = re.sub(r"\d", "", text)
         if random.random() < self.prob_shuffle_words:
             words = text.split()
             random.shuffle(words)
@@ -47,6 +58,8 @@ class TextAugmentation:
             words = text.split()
             words.pop(random.randint(0, len(words) - 1))
             text = " ".join(words)
+        if random.random() < self.ponctuation:
+            text = re.sub(r"[^\w\s]", "", text)
         return text
 
     def batch_call(self, texts: list):
@@ -65,13 +78,12 @@ def load_split_data(langs_dict):
         logging.warning("Saving Test Set...")
         test.save_to_disk(test_save_path)
         logging.warning("Test Set Saved.")
-    
+
     return train, valid, test
 
 
 def load_model_and_tokenizer():
-    model_path = "data/model"
-    if os.path.exists(model_path):
+    if os.path.exists("data/model"):
         with open("data/languages.json") as f:
             langs_dict = json.load(f)
 
@@ -80,13 +92,15 @@ def load_model_and_tokenizer():
         with open("data/model_config.json") as f:
             config_data: dict = json.load(f)
 
-        config_data.update({
-            "pad_token_id": tokenizer.pad_token_id,
-            "vocab_size": tokenizer.vocab_size,
-            "num_labels": len(langs_dict),
-            "label2id": langs_dict,
-            "id2label": {v: k for k, v in langs_dict.items()}
-        })
+        config_data.update(
+            {
+                "pad_token_id": tokenizer.pad_token_id,
+                "vocab_size": tokenizer.vocab_size,
+                "num_labels": len(langs_dict),
+                "label2id": langs_dict,
+                "id2label": {v: k for k, v in langs_dict.items()},
+            }
+        )
         config = BertConfig(**config_data)
 
         model = BertForSequenceClassification(config)
@@ -111,12 +125,22 @@ def main():
     augment_text = TextAugmentation()
 
     def make_augmented_text(examples):
-        examples["text"] = augment_text.batch_call(examples["text"]) if isinstance(examples["text"], list) else augment_text(examples["text"])
+        examples["text"] = (
+            augment_text.batch_call(examples["text"])
+            if isinstance(examples["text"], list)
+            else augment_text(examples["text"])
+        )
         return transform(examples)
 
     def transform(examples):
-        tokens = tokenizer(examples["text"], truncation=True, padding="max_length", return_tensors="pt")
-        tokens["label"] = torch.tensor([langs_dict[i] for i in examples["label"]]) if isinstance(examples["label"], list) else torch.tensor(langs_dict[examples["label"]])
+        tokens = tokenizer(
+            examples["text"], truncation=True, padding="max_length", return_tensors="pt"
+        )
+        tokens["label"] = (
+            torch.tensor([langs_dict[i] for i in examples["label"]])
+            if isinstance(examples["label"], list)
+            else torch.tensor(langs_dict[examples["label"]])
+        )
         return tokens
 
     # Apply transformations
@@ -128,7 +152,9 @@ def main():
     valid.set_transform(transform)
     test.set_transform(transform)
 
-    logging.info(f"Dataset Sizes - Train: {len(train)}, Valid: {len(valid)}, Test: {len(test)}")
+    logging.info(
+        f"Dataset Sizes - Train: {len(train)}, Valid: {len(valid)}, Test: {len(test)}"
+    )
 
     # Define training arguments
     training_args = TrainingArguments(
@@ -155,6 +181,7 @@ def main():
         bf16=True,
         torch_compile=True,
         save_total_limit=10,
+        metric_for_best_model="eval_loss",
     )
 
     metric = ComputeMetric(langs_dict)
@@ -167,6 +194,7 @@ def main():
         eval_dataset=valid,
         processing_class=tokenizer,
         compute_metrics=metric.compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
     )
 
     trainer.evaluate(valid)
